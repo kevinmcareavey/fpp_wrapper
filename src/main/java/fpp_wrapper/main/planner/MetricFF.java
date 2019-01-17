e36cbd46e07a672a777a7b672ad8d437274bacb1
Kevin McAreavey <>
2019-01-17 16:44:52 +0000

    Throw exeception on bad exit from Metric-FF
    Throw exeception when Metric-FF returns non-zero exit code.

--- src/main/java/fpp_wrapper/main/planner/MetricFF.java
+++ src/main/java/fpp_wrapper/main/planner/MetricFF.java
@@ -35,7 +35,7 @@ public class MetricFF extends Planner {
 		process.waitFor();
 		int exitStatus = process.exitValue();
 		if(exitStatus != 0) {
-//			throw new PlannerException("metric-ff exited with status " + exitStatus);
+			throw new PlannerException("metric-ff exited with status " + exitStatus);
 		}
 		
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
